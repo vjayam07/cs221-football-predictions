7b5619e8a8a2f41906255efefa889228c4558701
vjayam07 <virajjayam@gmail.com>
2023-12-01 20:05:40 -0800

    testing and prediction

--- main.py
+++ main.py
@@ -70,7 +70,12 @@ def training_data_init(football_data):
         score1 = float(df_games.iloc[idx]['Score'][:1])
         score2 = float(df_games.iloc[idx]['Score'][-1])
 
-        winner = 0 if xG_1*np.sqrt(2)+score1*np.sqrt(7) > xG_2*np.sqrt(2)+score2*np.sqrt(7) else 1 # tiebreak goes to team1
+        if xG_1*np.sqrt(2)+score1*np.sqrt(7) > xG_2*np.sqrt(2)+score2*np.sqrt(7):
+            winner = 0
+        elif xG_1*np.sqrt(2)+score1*np.sqrt(7) == xG_2*np.sqrt(2)+score2*np.sqrt(7):
+            winner = 0
+        else:
+            winner = 1 # tiebreak goes to team1
 
         arr = team1.split()
         team1 = arr[-1] + ' '
@@ -110,7 +115,35 @@ def compiled_dataset(football_database, encodings):
 
     return baseline_data
 
-# 3O. TRAINING MODEL!
+# 3O. NEW DATASET FOR UPCOMING MATCHES (STRICTLY FOR TESTING)
+def new_matches(football_data):
+    df_games = pd.read_csv('all_data/scores_fixtures_dataframe.csv')
+    test_features = []
+    test_matches = []
+
+    idx = 0
+    while idx < df_games.shape[0]:
+        if df_games.iloc[idx]['Date'] == '2023-12-13' or df_games.iloc[idx]['Date'] == '2023-12-12': 
+            team1 = df_games.iloc[idx]['Home']
+            team2 = df_games.iloc[idx]['Away']
+            
+            arr = team1.split()
+            team1 = arr[-1] + ' '
+            for i, s in enumerate(arr):
+                if i == len(arr)-2:
+                    team1 += s
+                    break
+                team1 += s + ' '
+
+            full_vec = np.concatenate((football_data[team1], football_data[team2]))
+            test_features.append(full_vec)
+            test_matches.append((team1, team2))
+        idx += 1
+    
+    return test_features, test_matches
+
+
+# 4O. TRAINING MODEL!
 def train(training_features, training_outputs, validation_features=None, validation_outputs=None):
     features_tensor, outputs_tensor = torch.Tensor(training_features), torch.Tensor(training_outputs)
     dataset = TensorDataset(features_tensor, outputs_tensor)
@@ -139,35 +172,58 @@ def train(training_features, training_outputs, validation_features=None, validat
         
     return model
 
-def test(model, test_features, test_outputs):
+def test(model, test_features, test_outputs=None):
     # Assuming test_features and test_labels are your test data
     test_features_tensor = torch.tensor(test_features, dtype=torch.float32)
-    test_labels_tensor = torch.tensor(test_outputs, dtype=torch.float32).view(-1, 1)
-
-    # Create a DataLoader for the test set
-    test_dataset = TensorDataset(test_features_tensor, test_labels_tensor)
-    test_loader = DataLoader(test_dataset, batch_size=32)  # Adjust batch size as needed
-
-    # Set model to evaluation mode
-    model.eval()
-
-    # Disable gradient calculations
     all_predictions = np.array([])
-    with torch.no_grad():
-        correct = 0
-        total = 0
-        for inputs, labels in test_loader:
-            outputs = model(inputs)
-            predicted = (outputs > 0.5).float()  # Assuming a threshold of 0.5 for binary classification
+    confidence = np.array([])
+    if test_outputs:
+        test_labels_tensor = torch.tensor(test_outputs, dtype=torch.float32).view(-1, 1)
 
-            new_predicted = torch.flatten(predicted)
-            all_predictions = np.concatenate((all_predictions, new_predicted.numpy()))
-            
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-    accuracy = correct / total
-    print(f'Accuracy of the model on the test set: {accuracy:.4f}')
-    return all_predictions
+        # Create a DataLoader for the test set
+        test_dataset = TensorDataset(test_features_tensor, test_labels_tensor)
+        test_loader = DataLoader(test_dataset, batch_size=16)  # Adjust batch size as needed
+
+        # Set model to evaluation mode
+        model.eval()
+
+        # Disable gradient calculations
+        with torch.no_grad():
+            correct = 0
+            total = 0
+            for inputs, labels in test_loader:
+                outputs = model(inputs)
+
+                new_outputs = torch.flatten(outputs)
+                confidence = np.concatenate((confidence, new_outputs.numpy()))
+
+                predicted = (outputs > 0.5).float()  # Assuming a threshold of 0.5 for binary classification
+                
+                new_predicted = torch.flatten(predicted)
+                all_predictions = np.concatenate((all_predictions, new_predicted.numpy()))
+                
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()      
+            accuracy = correct / total
+            print(f'Accuracy of the model on the test set: {accuracy:.4f}')
+    else:
+        test_loader = DataLoader(test_features_tensor, batch_size=14)
+        for inputs in test_loader:
+                outputs = model(inputs)
+                new_outputs = torch.flatten(outputs)
+                confidence = np.concatenate((confidence, new_outputs.detach().numpy()))
+
+                predicted = (outputs > 0.5).float()  # Assuming a threshold of 0.5 for binary classification
+                new_predicted = torch.flatten(predicted)
+
+                all_predictions = np.concatenate((all_predictions, new_predicted.numpy()))
+
+    
+    for idx, stat in enumerate(confidence):
+        stat = np.abs(stat-0.5) / 0.5 * 100
+        confidence[idx] = stat
+
+    return all_predictions, confidence
 
 def main(type):
     football_database = database_init()
@@ -184,19 +240,29 @@ def main(type):
     elif type == "oracle":
         training_features, training_outputs, test_features, test_outputs, test_matches = training_data_init(football_database)
         model = train(training_features, training_outputs)
-        all_predictions = test(model, test_features, test_outputs)
+        all_predictions, confidence = test(model, test_features, test_outputs=test_outputs)
         for idx, match in enumerate(test_matches):
             pred_winner = match[0] if all_predictions[idx] == 0 else match[1]
             true_winner = match[0] if test_outputs[idx] == 0 else match[1]
-            print("Match ", idx ,": ", match[0], " v.s. ", match[1])
-            print("Predicted Winnner is ", pred_winner)
+            print("Match ", idx+1 ,": ", match[0], " v.s. ", match[1])
+            print("Predicted Winnner is ", pred_winner, " with ", confidence[idx], "% confidence.")
             print("True Winner is ", true_winner)
+            if pred_winner == true_winner:
+                print("Correctly Guessed!")
             print("#################################")
-
-
-    elif type == "rand":
-        print(football_database)
-
+        
+        print("")
+        print("")
+        
+        new_features, upcoming_matches = new_matches(football_database)
+        all_predictions, confidence = test(model, new_features)
+        for idx, match in enumerate(test_matches):
+            pred_winner = match[0] if all_predictions[idx] == 0 else match[1]
+            print("Match ", idx+1 ,": ", match[0], " v.s. ", match[1])
+            print("Predicted Winnner is ", pred_winner, " with ", confidence[idx], "% confidence.")
+            print("#################################")
+            if idx == 15:
+                break
 
     else:
         print("please either input 'baseline' or 'oracle' into the main function.")
